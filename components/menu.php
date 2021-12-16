d00082f2e67b0f8ca412840008cdbb4c22a2dc1e
EnriqueLH <81501629+EnriqueLH@users.noreply.github.com>
2021-12-16 01:34:46 -0600

    Add files via upload

---
+++ components/menu.php
@@ -0,0 +1,10 @@
+<nav style="position: fixed;z-index: 100;top: 0;">
+  <div class="nav-wrapper black">
+    <a href="#!" class="brand-logo">
+      <img src="https://dsnegsjxz63ti.cloudfront.net/assets/img/push-notifications/icon.png" alt="logo" style="height: 60px">
+    </a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="index.php">Inicio</a></li>
+    </ul>
+  </div>
+</nav>
